7c0d64086936fbe58f0ea9f6fd7561c7f549ae23
r.ushakov <r.ushakov@qiwi.com>
2023-12-20 14:57:33 +0300

    add POJOs

---
+++ src/main/java/org/example/petclinic/model/Pet.java
@@ -0,0 +1,43 @@
+package org.example.petclinic.model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+public class Pet {
+    private String name;
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthdate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
---
+++ src/main/java/org/example/petclinic/model/PetType.java
@@ -0,0 +1,10 @@
+package org.example.petclinic.model;
+
+public class PetType {
+    private String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
